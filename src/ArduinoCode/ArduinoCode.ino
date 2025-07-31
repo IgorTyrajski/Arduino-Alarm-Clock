a999f8f05af951f17f6dede66c516069fffe8b3f
IgorTyrajski <igor.t@vp.pl>
2025-07-31 21:44:20 +0200

    Dodanie sterowania wyświetlacza 7-segmentowego + odpowiednio przerobione biblioteki pod sterowanie za pomocą 74HC595

--- src/ArduinoCode/ArduinoCode.ino
+++ src/ArduinoCode/ArduinoCode.ino
@@ -1,13 +1,24 @@
-#include <SevSeg.h>
-#include <LiquidCrystal_I2C.h>
+#include "SevSeg.h"
+int number=0;
+// Piny cyfr (common pins)
+const byte digitPins[] = {10,11, 12, 13}; // Przykładowe piny
 
+// Piny rejestru przesuwnego (SER, SRCLK, RCLK)
+const byte shiftRegisterPins[] = {7, 8, 9}; // Przykładowe piny
+
+SevSeg sevseg;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  sevseg.begin(COMMON_ANODE,4, digitPins, shiftRegisterPins);
+  sevseg.setBrightness(90);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sevseg.setNumber(number);
+  for(int i = 0; i < 100; i++) {
+    sevseg.refreshDisplay();
+    delay(1);
+  }
+  sevseg.refreshDisplay();
+  number++;
 }
